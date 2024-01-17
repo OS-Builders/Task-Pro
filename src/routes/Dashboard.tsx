110b634081aeb1b8ad1a61934fbcba213becdc87
John Costello <jlcos777@gmail.com>
2024-01-17 16:49:26 -0500

    additional comments

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -3,6 +3,8 @@ import MainContainer from "../containers/MainContainer";
 import { DashboardProps } from "../types";
 
 const Dashboard = ({ username }: DashboardProps) => {
+  //state of which board being selected (ID)
+  //state confirmDelete false
   return (
     <div className="main-page">
       <LeftContainer username={username} />
